c923f4ed168187397bdfb4172f7ea99669db6faa
Tobias235 <tobias.prepas@gmail.com>
2022-10-19 18:07:27 +0800

    Create LeftHomePage.js

---
+++ src/components/HomePageComponents/LeftHomePage/LeftHomePage.js
@@ -0,0 +1,11 @@
+import HomePageBackground from "./HomePageBackground/HomePageBackground";
+
+const LeftHomePage = () => {
+  return (
+    <>
+      <HomePageBackground />
+    </>
+  );
+};
+
+export default LeftHomePage;

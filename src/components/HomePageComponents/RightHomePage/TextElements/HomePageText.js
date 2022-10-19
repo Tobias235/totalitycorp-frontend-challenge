9e720df96892f4e9e61a8177190db1bf4013a463
Tobias235 <tobias.prepas@gmail.com>
2022-10-19 18:07:54 +0800

    added text component at right side of page

---
+++ src/components/HomePageComponents/RightHomePage/TextElements/HomePageText.js
@@ -0,0 +1,12 @@
+import styles from "./HomePageText.module.scss";
+
+const HomePageText = () => {
+  return (
+    <div className={styles.homePageText}>
+      <h1>Happening now</h1>
+      <h3>Join Twitter today.</h3>
+    </div>
+  );
+};
+
+export default HomePageText;

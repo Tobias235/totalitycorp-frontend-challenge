3a82bcd0c5993a230d3023c8795e5cdb4592811e
Tobias235 <tobias.prepas@gmail.com>
2022-10-20 17:32:39 +0800

    Update App.js
    Added darkmode functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useState, useEffect } from "react";
+
 import styles from "./App.module.scss";
 import Footer from "./components/Footer/Footer";
 import HomePageContainer from "./container/HomePageContainer";
 
 function App() {
+  const [mode, setMode] = useState();
+
+  useEffect(() => {
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (e) => {
+        let mode = e.matches ? "dark" : "light";
+        setMode(mode);
+        localStorage.setItem("darkMode", mode);
+      });
+  }, []);
+
+  useEffect(() => {
+    const mode = localStorage.getItem("darkMode");
+    console.log(mode);
+    setMode(mode);
+  }, []);
+
+  const darkMode = mode === "dark" ? styles.darkMode : styles.lightMode;
   return (
-    <div className={styles.app}>
-      <HomePageContainer />
+    <div className={darkMode}>
+      <HomePageContainer mode={mode} />
       <Footer />
     </div>
   );
